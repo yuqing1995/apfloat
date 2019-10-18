af9210952dbfcda51a651eed2460d09c8556e54c
Qing Yu <yuq1995@gmail.com>
2019-10-18 16:06:53 -0400

    add float2binary

--- apfloat.h
+++ apfloat.h
@@ -12,7 +12,7 @@ class apfloat
 {
     public:
         // apfloat Constructor 
-        apfloat() {}
+        apfloat() {};
         apfloat(const bool neg, const unsigned int exp, const unsigned int frac){
             if (exp > _exp.maxVal || frac > _frac.maxVal){
             cout << "Construction Failed, max exponent value: " 
@@ -26,7 +26,7 @@ class apfloat
         ~apfloat() {};
         // Operation Function 
         float binary2float();
-        bool float2binary(const float temp);
+        void float2binary(const float temp);
     private:
         // number of bits of exponent
         lint<le> _exp;
@@ -43,17 +43,57 @@ template<unsigned int le, unsigned int lf> float apfloat<le, lf>::binary2float()
     int e = _exp.val - bias;
     // Calculate the f
     int tmp_frac = _frac.val;
-    int f = 0;
-    for (int i = lf; i > 0; i++){
-        int reminder = tmp_frac % 2;
+    float f = 0.0;
+    int reminder = 0;
+    for (int i = lf; i > 0; i--){
+        reminder = tmp_frac % 2;
         tmp_frac = tmp_frac / 2;
         f += reminder * (1/pow(2, i));
     }
     // Use formular to calculate the result value based on _neg
-    return _neg == 1 ? float(0 - (pow(2, e) * (1 + f))) : float(pow(2, e) * (1 + f));
+    return _neg == 1 ? float(0.0 - (pow(2, e) * (1 + f))) : float(pow(2, e) * (1 + f));
 }
-template<unsigned int le, unsigned int lf> bool apfloat<le, lf>::float2binary(const float temp) {
-    return true;
+template<unsigned int le, unsigned int lf> void apfloat<le, lf>::float2binary(const float temp) {
+    int integer = int(temp);
+    float fp = temp - integer;
+    int bias = (_exp.maxVal + 1) / 2 - 1;
+    int size = log2(integer) + 1;
+    char floatBinary[lf]; 
+    // Construct the binary array of char with '0' and '1'
+    for (int i=0; i<size; i++){
+        floatBinary[i] = '0' + (integer % 2);
+        integer = integer / 2;
+    }
+    int exp = size - 1 + bias;
+    int frac = 0;
+    if (lf <= size - 1){
+        for (int i = lf; i >= 1; i--){
+            frac += (floatBinary[i]-'0') * pow(2, (lf-i));
+        }
+    }
+    else{
+        for(int i = size; i < lf-size; i++){
+            fp = 2 * fp;
+            if (fp > 1){
+                floatBinary[i] = '0'+1;
+                fp = fp - 1;
+            }else{
+                floatBinary[i] = '0';
+            }
+        }
+        for (int i = lf; i >= 1; i--){
+            frac += (floatBinary[i]-'0') * pow(2, (lf-i));
+        }
+    }
+    // Assign praivate variable
+    if (temp < 0){
+        _neg = 1;
+    }else{
+        _neg = 0;
+    }
+    _exp.val = exp;
+    _frac.val = frac;
+    cout << "neg: " << _neg<<" exp: "<<_exp.val<<" frac: "<<_frac.val<<endl;
 }
 
 
--- test_apfloat.cpp
+++ test_apfloat.cpp
@@ -5,9 +5,23 @@ using namespace std;
 
 int main()
 {
-   cout<<"Hello World\n";
-   apfloat<8, 13> ap_float(0, 124, 22);
-   cout<<"binary2float: " << ap_float.binary2float();
+   cout<<"APFloat Calculation\n";
+   // Test binary2float
+   apfloat<8, 7> ap_float1(1, 17, 8); // -8.1852e-34
+   apfloat<8, 7> ap_float2(0, 17, 80); // -1.25185e-33
+   apfloat<8, 7> ap_float3(1, 127, 8); // -1.0625
+   apfloat<8, 7> ap_float4(0, 127, 80); // 1.625
+   cout<<"binary2float:\nap_float1: " << ap_float1.binary2float() << endl;
+   cout<<"ap_float2: " << ap_float2.binary2float() << endl;
+   cout<<"ap_float3: " << ap_float3.binary2float() << endl;
+   cout<<"ap_float4: " << ap_float4.binary2float() << endl;
+   
+   // Test float2binary
+   apfloat<8, 7> ap_float5(0, 132, 54); // 45.25
+   apfloat<8, 7> ap_float6;
+   ap_float6.float2binary(45.45);
+   cout<<"float2binary:\nap_float5: " << ap_float5.binary2float() << endl;
+   
    return 0;
 }
 
